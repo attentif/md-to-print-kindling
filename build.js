518f0de1ce418c41c263da5fbd2105bdc501e58f
Simon Goumaz <simon@attentif.ch>
2015-03-17 13:02:30 +0100

    First implementation embryo (i.e. Metalsmith with simplistic Handlebars-Markdown compilation)

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
-var metalsmith = require('metalsmith')(__dirname),
+var debug = require('debug')('build'),
+    metalsmith = require('metalsmith')(__dirname),
     inPlace = require('metalsmith-in-place'),
-    markdown = require('metalsmith-markdown'),
+    path = require('path'),
+    Remarkable = require('remarkable'),
     watch = process.argv[2] === 'watch' ? require('metalsmith-watch') : null;
 
 metalsmith
@@ -11,6 +13,42 @@ metalsmith
       engine: 'handlebars'
     }))
     .use(markdown());
+
+function markdown() {
+  var md = new Remarkable('full', {
+    breaks: true,
+    typographer: true,
+    quotes: '«»‘’'
+  });
+
+  return function (files, metalsmith, done) {
+    Object.keys(files).forEach(function (file) {
+      debug('checking file: %s', file);
+      if (! isMarkdown(file)) {
+        return;
+      }
+
+      var data = files[file],
+          dirName = path.dirname(file),
+          htmlName = path.basename(file, path.extname(file)) + '.html';
+      if (dirName !== '.') {
+        htmlName = dirName + '/' + htmlName;
+      }
+
+      debug('converting file: %s', file);
+      var str = md.render(data.contents.toString());
+      data.contents = new Buffer(str);
+
+      delete files[file];
+      files[htmlName] = data;
+    });
+    done();
+  };
+}
+
+function isMarkdown(file){
+  return /\.md|\.markdown/.test(path.extname(file));
+}
 
 if (watch) {
   metalsmith.use(watch());
--- makefile
+++ makefile
@@ -2,6 +2,9 @@
 build: node_modules
 	node build.js
 
+build-verbose: node_modules
+	DEBUG=* node build.js
+
 watch: node_modules
 	node build.js watch
 
