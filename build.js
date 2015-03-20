016c3fe84bdc9c05df496946662b7d269bd30714
Simon Goumaz <simon@attentif.ch>
2015-03-20 16:24:18 +0100

    Replaced manual use of Remarkable with plugin

--- build.js
+++ build.js
@@ -1,8 +1,6 @@
-var debug = require('debug')('build'),
+var inPlace = require('metalsmith-in-place'),
+    markdown = require('metalsmith-markdown-remarkable'),
     metalsmith = require('metalsmith')(__dirname),
-    inPlace = require('metalsmith-in-place'),
-    path = require('path'),
-    Remarkable = require('remarkable'),
     watch = process.argv[2] === 'watch' ? require('metalsmith-watch') : null;
 
 metalsmith
@@ -12,43 +10,11 @@ metalsmith
     .use(inPlace({
       engine: 'handlebars'
     }))
-    .use(markdown());
-
-function markdown() {
-  var md = new Remarkable('full', {
-    breaks: true,
-    typographer: true,
-    quotes: '«»‘’'
-  });
-
-  return function (files, metalsmith, done) {
-    Object.keys(files).forEach(function (file) {
-      debug('checking file: %s', file);
-      if (! isMarkdown(file)) {
-        return;
-      }
-
-      var data = files[file],
-          dirName = path.dirname(file),
-          htmlName = path.basename(file, path.extname(file)) + '.html';
-      if (dirName !== '.') {
-        htmlName = dirName + '/' + htmlName;
-      }
-
-      debug('converting file: %s', file);
-      var str = md.render(data.contents.toString());
-      data.contents = new Buffer(str);
-
-      delete files[file];
-      files[htmlName] = data;
-    });
-    done();
-  };
-}
-
-function isMarkdown(file){
-  return /\.md|\.markdown/.test(path.extname(file));
-}
+    .use(markdown('full', {
+      breaks: true,
+      typographer: true,
+      quotes: '«»‘’'
+    }));
 
 if (watch) {
   metalsmith.use(watch());
