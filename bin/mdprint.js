a5204b6e662d340c2602778746f0982849576d46
Simon Goumaz <simon@attentif.ch>
2020-12-14 15:32:37 +0100

    Test CLI instead of lib

--- bin/mdprint.js
+++ bin/mdprint.js
@@ -3,16 +3,19 @@
 const yargs = require('yargs/yargs');
 const options = require('../lib/options');
 const printMarkdown = require('../lib/printMarkdown');
-const util = require('util');
+const process = require('process');
 
 const argv = yargs(process.argv.slice(2))
   .usage('Usage: $0 <Markdown file(s)> [options] --output <PDF file>')
   .example('$0 doc.md -o doc.pdf', 'Single Markdown file')
   .example('$0 ch1.md ch2.md ch3.md -o book.pdf', 'Multiple Markdown files (to be concatenated)')
-  .alias('o', 'output')
-  .nargs('o', 1)
-  .describe('o', 'Name or path of the output PDF file')
-  .demandOption(['o'])
+  .demandCommand(1, 'Please specify at least one Markdown file')
+  .option('o', {
+    alias: 'output',
+    describe: 'Name of the PDF file to generate',
+    nargs: 1,
+    defaultDescription: '<input>.pdf'
+  })
   .help('h')
   .alias('h', 'help')
   .alias('v', 'version')
@@ -23,7 +26,9 @@ const opts = options.parse();
 printMarkdown(argv._, argv.output, opts)
   .then(() => {
     console.log('OK');
+    process.exit(0);
   })
   .catch((err) => {
-    console.error(util.inspect(err));
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
   });
